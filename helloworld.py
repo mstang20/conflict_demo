957ea9195effca568c15e658eb64c35e8aee1f51
Justin Pritchard <mstang204fbl@yahoo.com>
2021-11-28 16:47:18 -0600

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
-
-
-def main():
-    hello_world('New York')
-=======
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
